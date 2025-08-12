84d8a330eb6ffa2b6a5106d987ecd844067fb792
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-08-12 12:44:24 +0200

    added a high score component

--- src/components/HighScore.vue
+++ src/components/HighScore.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
+import { ref, computed } from "vue";
 interface Props {
-  scores: { name: string; value: number }[];
+  scores: { name: string; value: number; date: Date }[];
 }
 const props = defineProps<Props>();
+const isHighScoreActive = ref(false);
+
+const highScoreArrow = computed(() => (isHighScoreActive.value ? "▼" : "▲"));
 </script>
 
 <template>
+  <div class="high-score-header">
+    <button @click="isHighScoreActive = !isHighScoreActive">
+      {{ highScoreArrow }}
+    </button>
+    <h2 class="green">High Scores</h2>
+  </div>
   <div class="high-score">
-    <h2>High Scores</h2>
-    <ul>
-      <li v-for="(score, index) in props.scores" :key="index">
-        {{ score.name }}: {{ score.value }}
-      </li>
-    </ul>
+    <div v-if="isHighScoreActive">
+      <ol v-if="props.scores.length > 0">
+        <li v-for="(score, index) in props.scores" :key="index">
+          {{ score.name }} : {{ score.value }} Poeng - {{ score.date }}
+        </li>
+      </ol>
+      <div v-else>
+        <h3>Ingen high scores</h3>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,6 +36,12 @@ const props = defineProps<Props>();
   color: #fff;
   padding: 1em;
   border-radius: 8px;
-  width: 250px;
+  width: 350px;
+}
+.high-score-header {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  gap: 0.5em;
 }
 </style>

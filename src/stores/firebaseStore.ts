8570aad90b0e2e9d0760e4aa6dbf7437d9c7664f
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-08-18 09:17:35 +0200

    x

--- src/stores/firebaseStore.ts
+++ src/stores/firebaseStore.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import { auth } from "@/services/firebase";
-import { onAuthStateChanged, type User } from "firebase/auth";
+import { onAuthStateChanged, type User, updateProfile } from "firebase/auth";
 
 export const useFirebaseStore = defineStore("firebase", () => {
   const user = ref<User | null>(null);
@@ -10,5 +10,11 @@ export const useFirebaseStore = defineStore("firebase", () => {
     user.value = u;
   });
 
-  return { user };
+  const updateUserProfile = async (profileData: { displayName: string; photoURL: string }) => {
+    if (user.value) {
+      await updateProfile(user.value, profileData);
+    }
+  };
+
+  return { user, updateUserProfile };
 });

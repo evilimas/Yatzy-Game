61400448de5eec204183a1803213e5331adf170f
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-08-19 10:04:14 +0200

    clean

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB9dqKoArRqK0bDt5XUdCydpRnVYSdGIlI",
@@ -15,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);

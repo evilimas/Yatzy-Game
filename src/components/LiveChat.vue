78c205a1f7459f7b12c556ea1b0ea97686f07db5
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-08-19 10:04:40 +0200

    x

--- src/components/LiveChat.vue
+++ src/components/LiveChat.vue
@@ -1,38 +1,65 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useFirebaseStore } from "../stores/firebaseStore";
 import type { User } from "firebase/auth";
+import { auth } from "@/services/firebase";
+
+const isChatActive = ref(false);
+
 const firebaseStore = useFirebaseStore();
 
-interface ChatMessage {
-  id: number;
-  user: string;
-  text: string;
-}
+// interface ChatMessage {
+//   id: number;
+//   user: string;
+//   text: string;
+// }
 
-const messages = ref<ChatMessage[]>([]);
-const newMessage = ref("");
+// const messages = ref(firebaseStore.messages || []);
+const newMessage = ref<string>("");
 
-const sendMessage = () => {
-  if (newMessage.value.trim()) {
-    messages.value.push({
-      id: Date.now(),
-      user: firebaseStore.user?.email || "User",
-      text: newMessage.value,
-    });
+const chatArrow = computed(() => (isChatActive.value ? "▲" : "▼"));
+
+// const sendMessage = () => {
+//   if (newMessage.value.trim()) {
+//     messages.value.push({
+//       id: Date.now(),
+//       user: firebaseStore.user?.email || "User",
+//       text: newMessage.value,
+//     });
+//     newMessage.value = "";
+//   }
+// };
+const postMessage = () => {
+  const messageBody = newMessage.value;
+  const user = auth.currentUser;
+
+  if (messageBody) {
+    firebaseStore.addMessageToDB(messageBody, user);
     newMessage.value = "";
   }
 };
 </script>
 <template>
-  <div>
-    <h2>Live Chat</h2>
-    <div class="messages">
-      <div class="message" v-for="msg in messages" :key="msg.id">
-        <strong>{{ msg.user }}:</strong> {{ msg.text }}
-      </div>
+  <div class="live-chat">
+    <div class="live-chat-header">
+      <button @click="isChatActive = !isChatActive">
+        {{ chatArrow }}
+      </button>
+      <h2 class="green">Live Chat</h2>
     </div>
-    <input v-model="newMessage" @keyup.enter="sendMessage" placeholder="Type your message..." />
+    <div v-if="isChatActive" class="chat-content">
+      <div v-if="firebaseStore.messages.length" class="messages">
+        <div class="message" v-for="msg in firebaseStore.messages" :key="msg.id">
+          <strong>{{ msg.displayName }}:</strong> {{ msg.text }} -
+          <em>{{ firebaseStore.displayDate(msg.createdAt) }}</em>
+        </div>
+      </div>
+      <div v-else class="no-chat">
+        <p>ingen meldinger</p>
+      </div>
+      <input v-model="newMessage" @keyup.enter="postMessage()" placeholder="Type your message..." />
+      <!-- <button @click="firebaseStore.fetchOnceAndRenderMessagesFromDB">Fetch Messages</button> -->
+    </div>
   </div>
 </template>
 
@@ -40,9 +67,28 @@ const sendMessage = () => {
 .messages {
   max-height: 300px;
   overflow-y: auto;
+  background: #222;
+  color: #f9f9f9;
 }
 .message {
   padding: 5px 10px;
   border-bottom: 1px solid #eee;
+}
+.live-chat {
+  background: #f9f9f9;
+  padding: 1em;
+  border-radius: 8px;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+}
+.live-chat-header {
+  display: flex;
+  align-items: center;
+}
+.no-chat {
+  text-align: center;
+  color: #1f1f1f;
+  font-weight: 800;
 }
 </style>
--- src/stores/firebaseStore.ts
+++ src/stores/firebaseStore.ts
@@ -2,13 +2,24 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { auth } from "@/services/firebase";
 import { onAuthStateChanged, type User, updateProfile } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc, serverTimestamp, getDocs, onSnapshot } from "firebase/firestore";
+import { Timestamp } from "firebase/firestore";
 
 export const useFirebaseStore = defineStore("firebase", () => {
+  const db = getFirestore();
+
   const user = ref<User | null>(null);
+  const messages = ref<
+    { id: string; user: string; text: string; createdAt: Timestamp; displayName: string }[]
+  >([]);
 
   onAuthStateChanged(auth, (u) => {
     user.value = u;
+    fetchInRealTimeAndRenderMessagesFromDB();
   });
+
+  //   update profile functions
 
   const updateUserProfile = async (profileData: { displayName: string; photoURL: string }) => {
     if (user.value) {
@@ -16,5 +27,91 @@ export const useFirebaseStore = defineStore("firebase", () => {
     }
   };
 
-  return { user, updateUserProfile };
+  //   Live chat functions
+  const addMessageToDB = async (message: string, user: User | null) => {
+    try {
+      const docRef = await addDoc(collection(db, "messages"), {
+        messageBody: message,
+        uid: user?.uid,
+        displayName: user?.displayName || user?.email,
+        createdAt: serverTimestamp(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  //   const fetchOnceAndRenderMessagesFromDB = async () => {
+  //     messages.value = [];
+  //     const querySnapshot = await getDocs(collection(db, "messages"));
+  //     querySnapshot.forEach((doc) => {
+  //       console.log(`${doc.id}: ${doc.data().messageBody}`);
+  //       messages.value.unshift({
+  //         // displayName: doc.data().name,
+  //         id: doc.id,
+  //         user: doc.data().uid,
+  //         displayName: doc.data().displayName,
+  //         text: doc.data().messageBody,
+  //         createdAt: doc.data().createdAt,
+  //       });
+  //     });
+  //   };
+
+  const fetchInRealTimeAndRenderMessagesFromDB = async () => {
+    onSnapshot(collection(db, "messages"), (querySnapshot) => {
+      messages.value = [];
+      querySnapshot.forEach((doc) => {
+        messages.value.unshift({
+          // displayName: doc.data().name,
+          id: doc.id,
+          user: doc.data().uid,
+          displayName: doc.data().displayName,
+          text: doc.data().messageBody,
+          createdAt: doc.data().createdAt,
+        });
+      });
+    });
+  };
+
+  const displayDate = (firebaseDate: Timestamp) => {
+    if (!firebaseDate) return "Date processing";
+    const date = firebaseDate.toDate();
+
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const month = monthNames[date.getMonth()];
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const paddedHours = hours < 10 ? "0" + hours : hours.toString();
+    const paddedMinutes = minutes < 10 ? "0" + minutes : minutes.toString();
+
+    return `${day} ${month} ${year} - ${paddedHours}:${paddedMinutes}`;
+  };
+
+  return {
+    user,
+    messages,
+    updateUserProfile,
+    addMessageToDB,
+    // fetchOnceAndRenderMessagesFromDB,
+    fetchInRealTimeAndRenderMessagesFromDB,
+    displayDate,
+  };
 });

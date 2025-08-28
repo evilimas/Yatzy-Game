e3fd413b297fa850e175e0ed1d08ab21387b3c41
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-08-28 09:35:15 +0200

    made multipayer vue

--- src/views/YatzyMPView.vue
+++ src/views/YatzyMPView.vue
@@ -1,10 +1,27 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+</script>
 
 <template>
+  <nav class="navbar">
+    <button @click="router.push('/home')">Tilbake</button>
+  </nav>
   <div>
     <h1>Yatzy Multiplayer</h1>
     <p>Velkommen til Yatzy Multiplayer!</p>
   </div>
 </template>
 
-<style></style>
+<style>
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background: #222;
+  color: #fff;
+  padding: 0.8em;
+  border-bottom: 2px solid #444;
+}
+</style>

72f0d00b59f52718000c9506337d489ffb4b3331
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-10-02 09:28:11 +0200

    create account display name bug fix

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -62,7 +62,7 @@ watch(
     <section id="logged-out-view" v-if="!loginPage">
       <h2>Opprett en konto</h2>
       <div class="container">
-        <form action="">
+        <form action="" @submit.prevent>
           <input v-model="name" id="create-name-input" type="text" placeholder="Navn" required />
           <input
             v-model="email"
